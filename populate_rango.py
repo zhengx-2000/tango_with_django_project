f50f957451a15506710840d0585264a6cf59525a
Xiao Zheng <2773671z@student.gla.ac.uk>
2023-01-27 21:56:21 +0000

    Chapter 6 tutorial completed

--- populate_rango.py
+++ populate_rango.py
@@ -1,10 +1,13 @@
 import os
-import django
-from rango.models import Category, Page
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
+
+import django
+
 django.setup()
+
+from rango.models import Category, Page
 
 
 def populate():
--- rango/admin.py
+++ rango/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)

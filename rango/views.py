3d102637d7d0cee989cf047050d12d256d9f5842
Xiao Zheng <2773671z@student.gla.ac.uk>
2023-02-02 00:31:18 +0000

    Chapter 8 tutorial completed

--- rango/views.py
+++ rango/views.py
@@ -25,6 +25,10 @@ def index(request):
 
 
 def about(request):
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name. if no one is logged in it prints 'AnonymousUser'
+    print(request.user)
     return render(request, 'rango/about.html')
 
 

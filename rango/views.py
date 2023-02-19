f5163f6de5a0d079123aff02f7282750e858cf5e
Xiao Zheng <2773671z@student.gla.ac.uk>
2023-02-19 23:29:36 +0000

    Chapter 10 tutorial completed

--- rango/views.py
+++ rango/views.py
@@ -33,7 +33,7 @@ def index(request):
 def about(request):
     # prints out whether the method is a GET or a POST
     print(request.method)
-    # prints out the user name. if no one is logged in it prints 'AnonymousUser'
+    # prints out the username. if no one is logged in it prints 'AnonymousUser'
     print(request.user)
 
     # Cookie Test

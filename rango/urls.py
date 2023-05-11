3618c7f3e4a16bb0a3317a44587fe0c8d2bc9b49
Xiao Zheng <2773671z@student.gla.ac.uk>
2023-05-12 00:57:56 +0100

    Chapter 17 completed

--- rango/urls.py
+++ rango/urls.py
@@ -19,5 +19,4 @@ urlpatterns = [
     path('like_category/', views.LikeCategoryView.as_view(), name='like_category'),
     path('suggest/', views.CategorySuggestionView.as_view(), name='suggest'),
     path('search_add_page/', views.SearchAddPageView.as_view(), name='search_add_page'),
-
 ]
